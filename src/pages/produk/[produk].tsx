8f8ed9343b8cdbd27c719c1c3d6fc6c1951c1be2
sirfara <aratihsirf@gmail.com>
2026-04-23 22:29:14 +0700

    initial deployment1

--- src/pages/produk/[produk].tsx
+++ src/pages/produk/[produk].tsx
@@ -15,35 +15,48 @@ const HalamanProduk = ({ product }: { product: ProductType }) => {
 
 export default HalamanProduk;
 
-// export async function getServerSideProps({ ...
-// }
-
-export async function getStaticPaths() {
-  const res = await fetch("http://localhost:3000/api/produk");
-  const response = await res.json();
-
-  const paths = response.data.map((product: ProductType) => ({
-    params: { produk: product.id },
-  }));
-
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export async function getStaticProps({
-  params,
-}: {
-  params: { produk: string };
-}) {
-  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
-  const response: { data: ProductType } = await res.json();
+export async function getServerSideProps({ params }: { params: { produk: string } }) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/produk/${params?.produk}`);
+  const respone = await res.json();
+  
+  // console.log("Data produk yang diambil dari API:", respone);
 
   return {
     props: {
-      product: response.data,
+      product: respone.data, // Pastikan untuk memberikan nilai default jika data tidak tersedia
     },
-    revalidate: 10,
   };
 }
+
+// export async function getServerSideProps({ ...
+// }
+
+// export async function getStaticPaths() {
+//   const res = await fetch("http://localhost:3000/api/produk");
+//   const response = await res.json();
+
+//   const paths = response.data.map((product: ProductType) => ({
+//     params: { produk: product.id },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
+
+// export async function getStaticProps({
+//   params,
+// }: {
+//   params: { produk: string };
+// }) {
+//   const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+//   const response: { data: ProductType } = await res.json();
+
+//   return {
+//     props: {
+//       product: response.data,
+//     },
+//     revalidate: 10,
+//   };
+// }
--- src/pages/produk/server.tsx
+++ src/pages/produk/server.tsx
@@ -15,7 +15,7 @@ export default HalamanProdukServer;
 
 export async function getServerSideProps() {
 
-  const res = await fetch("http://localhost:3000/api/produk");
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk`);
   const response = await res.json();
 
   return {
